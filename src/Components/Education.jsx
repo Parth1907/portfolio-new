7b9e7fbe077b2c1bc16c4d6c7989d932e2f4f8c0
Parth Kalra <93028052+Parth1907@users.noreply.github.com>
2024-07-03 11:09:41 +0000

    added style and edu

---
+++ src/Components/Education.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <h3>12th CBSE, Summer Fields School </h3>
+      2021
+    </section>
+  );
+};
+
+export default Education;
